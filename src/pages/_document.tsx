2fb7d60b410336fefe2a7a743c01d222282d9863
iammarkps <markkung08@gmail.com>
2020-02-11 23:06:59 +0700

    :mag: Improve lighthouse score

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -22,12 +22,16 @@ class NextSite extends Document {
           <meta name="theme-color" content="#ffffff" />
           <meta
             name="viewport"
-            content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"
+            content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=5.0, user-scalable=yes"
           />
           <meta name="theme-color" content="black" />
           <meta property="og:title" content="I AM MARK" />
           <meta property="og:type" content="website" />
           <meta property="og:url" content="http://iammark.me" />
+          <meta
+            name="description"
+            content="นิทรรศการวิชาการ โรงเรียนเตรียมอุดมศึกษา ประจำปี ๒๕๖๒"
+          />
         </Head>
         <body>
           <Main />
